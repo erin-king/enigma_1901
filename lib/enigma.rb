f0d9f70f13d1a6dc8b36b8879249a9a766d9fdb4
Erin King <eohone@gmail.com>
2019-02-23 16:38:57 -0700

    integreation test test_it_encrypts_a_message passing with Enigma#encrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,11 +1,12 @@
 require 'pry'
 require 'date'
+require './lib/encrypt'
 
 class Enigma
 
   def encrypt(message, key = nil, date = nil)
-
-    #return {encryption: "blahblah", key: "12345", date: "012219"}
+    encryptor = Encrypt.new(key, date)
+    encryptor.encrypt(message)
   end
 
   def decrypt(cyphertext, key, date = nil)
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -13,64 +13,12 @@ class EngimaTest < Minitest::Test
     assert_instance_of Enigma, enigma
   end
 
-  def test_it_encrypts_message_with_key_and_date
-    skip
+  def test_it_encrypts_a_message
     enigma = Enigma.new
-
-    expected = {
-      encryption: "keder ohulw",
-      key: "02715",
-      date: "040895"
-    }
-    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
+    encryption = enigma.encrypt("hello world", "02715", "040895")
+    assert_equal "keder ohulw", encryption[:encryption]
   end
 
-  def test_it_decrypts_message_with_a_key_and_date
-    skip
-    enigma = Enigma.new
 
-    expected = {
-      decryption: "hello world",
-      key: "02715",
-      date: "040895"
-    }
-    assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
-  end
-
-  def test_it_encrypts_message_with_key_and_todays_date
-    skip
-    enigma = Enigma.new
-
-    expected = {
-      encryption: "keder ohulw",
-      key: "02715",
-      date: "042219" #today's date
-    }
-    assert_equal expected, enigma.encrypt("hello world", "02715")
-  end
-
-  def test_it_decrypts_message_with_a_key_and_todays_date
-    skip
-    enigma = Enigma.new
-
-    expected = {
-      decryption: "hello world",
-      key: "02715",
-      date: "040895" #today's date
-    }
-    assert_equal expected, enigma.decrypt(encrypted[:encryption], "02715")
-  end
-
-  def test_it_encrypts_message_with_generated_random_key_and_todays_date
-    skip
-    enigma = Enigma.new
-
-    expected = {
-      encryption: "keder ohulw",
-      key: "02715", #generated random number
-      date: "042219" #today's date
-    }
-    assert_equal expected, enigma.encrypt("hello world")
-  end
 
 end

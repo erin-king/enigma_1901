0cbd4c4d2c6b3474e6bd38b25ac8ca4b693fa502
Erin King <eohone@gmail.com>
2019-02-23 11:34:34 -0700

    add test_it_exists and #initialize

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,4 +2,8 @@ require 'pry'
 
 class Encrypt
 
+  def initialize(message, key = nil, date = nil)
+
+  end
+
 end
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -8,4 +8,33 @@ require './lib/encrypt'
 
 class EncryptTest < Minitest::Test
 
+  def test_it_exists
+    encrypt = Encrypt.new("MEAN cats!", "01234", "112982")
+    assert_instance_of Encrypt, encrypt
+  end
+
 end
+
+#tests
+#with all arguments provided
+#with no date
+#with no key
+#with no key or date
+#message with capital letters "MEAN cats"
+#message with characters "MEAN cats!"
+
+# def encrypt(message, key = nil, date = nil)
+#   #return {encryption: "blahblah", key: "12345", date: "012219"}
+# end
+#
+# def test_it_encrypts_message_with_key_and_date
+#   skip
+#   enigma = Enigma.new
+#
+#   expected = {
+#     encryption: "keder ohulw",
+#     key: "02715",
+#     date: "040895"
+#   }
+#   assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
+# end

8d49e56c657df93b79e7680aa7669c104f0ca5a1
Erin King <eohone@gmail.com>
2019-02-23 10:30:12 -0700

    add test and #generate_key, included in initialize

--- lib/key.rb
+++ lib/key.rb
@@ -5,7 +5,14 @@ class Key
   attr_reader :key
 
   def initialize(key)
-    @key = key
+    @key = generate_key(key)
+  end
+
+  def generate_key(key)
+    if key == nil
+      key = rand(99999)
+    end
+    key.to_s.rjust(5, "0")
   end
 
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -16,4 +16,10 @@ class KeyTest < Minitest::Test
   def test_it_has_a_key
     assert_equal "01234", @key.key
   end
+
+  def test_it_generates_random_key_if_not_provided
+    key = Key.new(nil)
+    refute nil, key.key
+    assert_equal 5, key.generate_key(nil).length
+  end
 end

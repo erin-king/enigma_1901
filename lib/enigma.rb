5a8b2e583bb60a956951ffd73bd7af7ef1094499
Erin King <eohone@gmail.com>
2019-02-23 08:59:24 -0700

    changed default argument values to nil for encrypt and decrypt methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,11 +3,11 @@ require 'date'
 
 class Enigma
 
-  def encrypt(message, key = 0, date = Date.today)
+  def encrypt(message, key = nil, date = nil)
     #return {encryption: "blahblah", key: "12345", date: "012219"}
   end
 
-  def decrypt(cyphertext, key, date = Date.today)
+  def decrypt(cyphertext, key, date = nil)
     #return {cyphertext: "whejdhdh", key: "12345", date: "012219"}
   end
 

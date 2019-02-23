2d8343f75c9464ed58dd84732bbdec4043bc6580
Erin King <eohone@gmail.com>
2019-02-23 09:15:26 -0700

    added tests for format_date

--- lib/offset.rb
+++ lib/offset.rb
@@ -5,8 +5,16 @@ class Offset
 
   attr_reader :date
 
-  def initialize(date = Date.today)
+  def initialize(date)
     @date = date
+  end
+
+  def format_date(date)
+    if date == nil
+      date = Date.today
+    end
+    date
+    # return date - this method goes in intialize
   end
 
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -7,7 +7,7 @@ require './lib/offset'
 class OffsetTest < Minitest::Test
 
   def setup
-    @offset = Offset.new(112982)
+    @offset = Offset.new("112982")
   end
 
   def test_it_exists
@@ -15,12 +15,24 @@ class OffsetTest < Minitest::Test
   end
 
   def test_it_has_a_date_when_provided
-    assert_equal 112982, @offset.date
+    assert_equal "112982", @offset.date
   end
 
   def test_it_has_todays_date_when_date_not_provided
-    offset = Offset.new
-    assert_equal 01231, offset.date
+    offset = Offset.new(nil)
+    assert_equal "012319", offset.date
+  end
+
+  def test_it_can_format_date_when_given_nil
+    offset = Offset.new(nil)
+
+    assert_equal "012319", offset.format_date(nil)
+  end
+
+  def test_format_date_returns_argument_date_if_date_is_not_nil
+    offset = Offset.new("112982")
+
+    assert_equal "112982", offset.format_date("112982")
   end
 
 

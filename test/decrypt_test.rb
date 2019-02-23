5dac61e0b5129d12135867cd0c8eefbad9dc6b91
Erin King <eohone@gmail.com>
2019-02-23 16:01:37 -0700

    add test_it_can_find_a_letters_index and find_letter_index

--- test/decrypt_test.rb
+++ test/decrypt_test.rb
@@ -15,4 +15,17 @@ class DecryptTest < Minitest::Test
   def test_it_exists
     assert_instance_of Decrypt, @decrypt
   end
+
+  def test_it_can_decrypt_message
+    assert_equal "abc abc ", @decrypt.decrypt_message("dqakdqak")
+  end
+
+  def test_it_can_decrypt_message_returning_non_alphabet_characters_as_is
+    assert_equal "abc abc !", @decrypt.decrypt_message("dqakdqak!")
+  end
+
+  def test_it_can_find_a_letters_index
+    assert_equal 2, @encrypt.find_letter_index("c")
+  end
+
 end
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -50,27 +50,3 @@ class EncryptTest < Minitest::Test
   end
 
 end
-
-#tests
-#with all arguments provided
-#with no date
-#with no key
-#with no key or date
-#message with capital letters "MEAN cats"
-#message with characters "MEAN cats!"
-
-# def encrypt(message, key = nil, date = nil)
-#   #return {encryption: "blahblah", key: "12345", date: "012219"}
-# end
-#
-# def test_it_encrypts_message_with_key_and_date
-#   skip
-#   enigma = Enigma.new
-#
-#   expected = {
-#     encryption: "keder ohulw",
-#     key: "02715",
-#     date: "040895"
-#   }
-#   assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
-# end

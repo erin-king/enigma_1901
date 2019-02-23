7b200f6f414819de7e9ecca8811817c8c3644c23
Erin King <eohone@gmail.com>
2019-02-23 10:52:23 -0700

    add test setup, test_it_exists and #initialize

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,6 +1,12 @@
 require 'pry'
+require './lib/key'
+require './lib/offset'
 
 class Shift
 
+  def initialize(key, offset)
+    @key = key
+    @offset = offset
+  end
 
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -7,4 +7,13 @@ require './lib/shift'
 
 class ShiftTest < Minitest::Test
 
+  def setup
+    @offset = Offset.new("112982")
+    @key = Key.new("01234")
+    @shift = Shift.new(@key, @offset)
+  end
+
+  def test_it_exists
+    assert_instance_of Shift, @shift
+  end
 end

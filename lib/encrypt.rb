d9c213a22cdc93cdf11fc1a61a568c7637835f68
Erin King <eohone@gmail.com>
2019-02-23 11:44:53 -0700

    completed #initialize to take in arguments and use shift, key and offset classes

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,9 +1,13 @@
 require 'pry'
+require './lib/key'
+require './lib/offset'
+require './lib/shift'
 
 class Encrypt
 
   def initialize(message, key = nil, date = nil)
-
+    @message = message
+    @shift = Shift.new(Key.new(key), Offset.new(date))
   end
 
 end
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -1,17 +1,11 @@
 require './test/test_helper'
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/key'
-require './lib/offset'
-require './lib/shift'
 require './lib/encrypt'
 
 class EncryptTest < Minitest::Test
 
   def setup
-    @offset = Offset.new("112982")
-    @key = Key.new("01234")
-    @shift = Shift.new(@key, @offset)
     @encrypt = Encrypt.new("MEAN cats!", "01234", "112982")
   end
 

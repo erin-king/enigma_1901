bceb139e478a0a058c83c3d7a0c7d516a660c6f2
Erin King <eohone@gmail.com>
2019-02-23 08:30:34 -0700

    added test_it_has_a_date_when_provided and attr_reader :date

--- lib/offset.rb
+++ lib/offset.rb
@@ -3,6 +3,8 @@ require 'date'
 
 class Offset
 
+  attr_reader :date
+
   def initialize(date = Date.today)
     @date = date
   end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -12,5 +12,10 @@ class OffsetTest < Minitest::Test
     assert_instance_of Offset, offset
   end
 
+  def test_it_has_a_date_when_provided
+    offset = Offset.new(112982)
+    assert_equal 112982, offset.date
+  end
+
 
 end

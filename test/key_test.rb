ef14010c8a56895aa447eef6e262e626932c8c54
Erin King <eohone@gmail.com>
2019-02-23 10:38:50 -0700

    add test and #key_a thru d

--- test/key_test.rb
+++ test/key_test.rb
@@ -22,4 +22,12 @@ class KeyTest < Minitest::Test
     refute nil, key.key
     assert_equal 5, key.generate_key(nil).length
   end
+
+  def test_it_can_generate_keys_a_through_d
+    assert_equal 01, @key.key_a
+    assert_equal 12, @key.key_b
+    assert_equal 23, @key.key_c
+    assert_equal 34, @key.key_d
+  end
+
 end

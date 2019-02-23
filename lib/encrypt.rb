d8be57f0d5da49e42cf756ffbdb7c01768832c8a
Erin King <eohone@gmail.com>
2019-02-23 15:28:06 -0700

    refactored #encrypt_message

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -24,22 +24,42 @@ class Encrypt
     encryption = []
     counter = 0
     message.split(//).each do |letter|
-      if counter == 0
-        encryption << apply_shift_to_letter(@shift.shift_a, letter)
-      elsif counter == 1
-        encryption << apply_shift_to_letter(@shift.shift_b, letter)
-      elsif counter == 2
-        encryption << apply_shift_to_letter(@shift.shift_c, letter)
-      elsif counter == 3
-        encryption << apply_shift_to_letter(@shift.shift_d, letter)
-      else counter == 4
-        counter = 0
-        encryption << apply_shift_to_letter(@shift.shift_a, letter)
-      end
       counter += 1
+      if counter == 1
+        encryption << apply_shift_to_letter(@shift.shift_a, letter)
+      elsif counter == 2
+        encryption << apply_shift_to_letter(@shift.shift_b, letter)
+      elsif counter == 3
+        encryption << apply_shift_to_letter(@shift.shift_c, letter)
+      elsif counter == 4
+        encryption << apply_shift_to_letter(@shift.shift_d, letter)
+        counter = 0
+      end
     end
     encryption.join
   end
+
+  #
+  # def encrypt_message(message)
+  #   encryption = []
+  #   counter = 0
+  #   message.split(//).each do |letter|
+  #     if counter == 0
+  #       encryption << apply_shift_to_letter(@shift.shift_a, letter)
+  #     elsif counter == 1
+  #       encryption << apply_shift_to_letter(@shift.shift_b, letter)
+  #     elsif counter == 2
+  #       encryption << apply_shift_to_letter(@shift.shift_c, letter)
+  #     elsif counter == 3
+  #       encryption << apply_shift_to_letter(@shift.shift_d, letter)
+  #     else counter == 4
+  #       counter = 0
+  #       encryption << apply_shift_to_letter(@shift.shift_a, letter)
+  #     end
+  #     counter += 1
+  #   end
+  #   encryption.join
+  # end
 
   def apply_shift_to_letter(shift, letter)
     (@alphabet.rotate(shift + find_letter_index(letter))).flatten[0]

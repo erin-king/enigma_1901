58ee4235478cb8c0a88214171ff86a7dff15d66a
Erin King <eohone@gmail.com>
2019-02-23 18:30:23 -0700

    completed decrypt runner

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -4,13 +4,13 @@ enigma = Enigma.new
 
 # CLI format: ruby ./lib/encrypt.rb message.txt encrypted.txt
 
-input_1 = ARGV[0]
-input_2 = ARGV[1]
+read_file = ARGV[0]
+write_file = ARGV[1]
 
-File.open("#{input_1}", "r") do |file|
+File.open("#{read_file}", "r") do |file|
   file.each_line do |line|
     enigma_hash = enigma.encrypt(line)
-    puts "Created '#{input_2}' with the key #{enigma_hash[:key]} and date #{enigma_hash[:date]}."
-    File.open(input_2, 'w') { |write_file| write_file.write(enigma_hash[:encryption]) }
+    puts "Created '#{write_file}' with the key #{enigma_hash[:key]} and date #{enigma_hash[:date]}."
+    File.open(write_file, 'w') { |w_file| w_file.write(enigma_hash[:encryption]) }
   end
 end

8ed94f9fe43cd57c93872138b10f89be5bc90744
Erin King <eohone@gmail.com>
2019-02-23 18:22:42 -0700

    moved message.txt at /lib /test level, completed encrypt runner

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,7 +1,22 @@
 require './lib/enigma'
 
-# $ ruby ./lib/decrypt.rb encrypted.txt decrypted.txt 82648 240818
+enigma = Enigma.new
+
+# CLI format: ruby ./lib/decrypt.rb encrypted.txt decrypted.txt 82648 240818
+
+input_1 = ARGV[0]
+input_2 = ARGV[1]
+input_3 = ARGV[2]
+input_4 = ARGV[3]
+
 # Created 'decrypted.txt' with the key 82648 and date 240818
 
+# The first is an existing file that contains an encrypted message. The second is a file where your program should write the decrypted message. The third is the key to be used for decryption. The fourth is the date to be used for decryption. In addition to writing the decrypted message to the file, your program should output to the screen the file it wrote to, the key used for decryption, and the date used for decryption.
 
-# Additionally, you should create a Runner file called decrypt.rb that takes four command line arguments. The first is an existing file that contains an encrypted message. The second is a file where your program should write the decrypted message. The third is the key to be used for decryption. The fourth is the date to be used for decryption. In addition to writing the decrypted message to the file, your program should output to the screen the file it wrote to, the key used for decryption, and the date used for decryption.
+File.open('lib/message.txt', "r") do |file|
+  file.each_line do |line|
+    enigma_hash = enigma.encrypt(line)
+    puts "Created '#{input_2}' with the key #{enigma_hash[:key]} and date #{enigma_hash[:date]}."
+    File.open(input_2, 'w') { |write_file| write_file.write(enigma_hash[:encryption]) }
+  end
+end
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,10 +2,12 @@ require './lib/enigma'
 
 enigma = Enigma.new
 
+# CLI format: ruby ./lib/encrypt.rb message.txt encrypted.txt
+
 input_1 = ARGV[0]
 input_2 = ARGV[1]
 
-File.open('lib/message.txt', "r") do |file|
+File.open("#{input_1}", "r") do |file|
   file.each_line do |line|
     enigma_hash = enigma.encrypt(line)
     puts "Created '#{input_2}' with the key #{enigma_hash[:key]} and date #{enigma_hash[:date]}."
